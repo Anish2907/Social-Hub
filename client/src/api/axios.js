7bccef4113b147d631ae2ee8720f840476dcdd49
Anish2907 <anishmondal225@gamil.com>
2024-01-29 03:21:58 +0530

    Update backend url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 export default axios.create({
     withCredentials: true,
-    baseURL: "http://localhost:8000"
+    baseURL: "https://social-hub-server-ebsq.onrender.com"
 });
 
 export const axiosPrivate = axios.create({
     withCredentials: true,
     headers: { "Content-Type": "application/json" },
-    baseURL: "http://localhost:8000"
+    baseURL: "https://social-hub-server-ebsq.onrender.com"
 });
