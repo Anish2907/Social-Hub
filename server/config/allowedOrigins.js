06a2deebd57a67145e02aba8788b976b2526fa0c
Anish2907 <anishmondal225@gamil.com>
2024-01-29 03:07:56 +0530

    Deploy backend

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,4 +1,4 @@
 export const allowedOrigins = [
     "http://localhost:3000",
-    "https://65b6c7c7d130b8a9a5e10585--preeminent-quokka-52b4f4.netlify.app"
+    "https://preeminent-quokka-52b4f4.netlify.app"
 ];
