e54d818ee89ae763703a468a2d1f74387e6e063b
Anish2907 <anishmondal225@gamil.com>
2024-04-10 15:43:36 +0530

    new server

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
     withCredentials: true,
-    baseURL: "https://social-hub-server.netlify.app/"
+    baseURL: "https://social-hub-server.netlify.app"
     // baseURL: "https://social-hub-server-ebsq.onrender.com"
     // baseURL: "http://localhost:8000"
 });
@@ -10,7 +10,7 @@ export default axios.create({
 export const axiosPrivate = axios.create({
     withCredentials: true,
     headers: { "Content-Type": "application/json" },
-    baseURL: "https://social-hub-server.netlify.app/"
+    baseURL: "https://social-hub-server.netlify.app"
     // baseURL: "https://social-hub-server-ebsq.onrender.com"
     // baseURL: "http://localhost:8000"
 });
