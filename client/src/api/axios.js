6190cb310ccb3bf65bcf698e66168c5aee38fb73
Anish2907 <anishmondal225@gamil.com>
2024-07-23 17:56:06 +0530

    Add new backend server

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,7 +3,8 @@ import axios from "axios";
 export default axios.create({
     withCredentials: true,
     // baseURL: "https://social-hub-server.netlify.app"
-    baseURL: "https://social-hub-server-ebsq.onrender.com"
+    // baseURL: "https://social-hub-server-ebsq.onrender.com"
+    baseURL: "https://social-hub-ochre.vercel.app/"
     // baseURL: "http://localhost:8000"
 });
 
@@ -11,6 +12,7 @@ export const axiosPrivate = axios.create({
     withCredentials: true,
     headers: { "Content-Type": "application/json" },
     // baseURL: "https://social-hub-server.netlify.app"
-    baseURL: "https://social-hub-server-ebsq.onrender.com"
+    // baseURL: "https://social-hub-server-ebsq.onrender.com"
+    baseURL: "https://social-hub-ochre.vercel.app/"
     // baseURL: "http://localhost:8000"
 });
